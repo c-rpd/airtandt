50a74c497fe53df54f866d1ca63fcdb145797444
léopold leclercq <leopold.leclercq@audencia.com>
2024-02-28 12:11:19 +0100

    tanks home page en cours

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    all_tanks = Tank.all
+    @tanks = all_tanks.take(3)
   end
 end
