10c0d4b9b110a2f9de1ae7f842d36b449b43a6bf
Corentin Ripaud <corentin.ripaud@gmail.com>
2024-03-01 12:01:44 +0100

    profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reservations, dependent: :destroy
   has_many :tanks, dependent: :destroy, foreign_key: :owner_id
-  has_many :reserved_tanks, through: :reservations, source: :tank
 end
